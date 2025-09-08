c8a17cc20896b02d6798963d5d9bac088687281f
ArulKJ <arulkj07@gmail.com>
2025-09-07 17:23:03 -0700

    Tweaked tmux and nvim edges

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -29,7 +29,7 @@ return {
             },
             sections = {
                 lualine_a = {
-                    { "mode", separator = { left = "", right = "" }, icon = "" },
+                    { "mode", separator = { left = "█", right = "" }, icon = "" },
                     -- { "mode", separator = { left = "█", right = " " }, icon = "" },
                 },
                 lualine_b = {
@@ -60,7 +60,7 @@ return {
                 },
                 lualine_y = { clients_lsp },
                 lualine_z = {
-                    { "location", separator = { left = "", right = "" }, icon = "" },
+                    { "location", separator = { left = "", right = "█" }, icon = "" },
                     -- { "location", separator = { left = " ", right = "█" }, icon = "" },
                 },
             },
